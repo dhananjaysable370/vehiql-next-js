7d4efb3a4c182082c97c32a6a366e1a5eccb910f
Dhananjay Sable <dhananjaysable370@gmail.com>
2025-03-26 17:52:54 +0530

    vehiql nav

---
+++ components/Header.jsx
@@ -0,0 +1,26 @@
+
+import Image from 'next/image';
+import Link from 'next/link';
+import React from "react";
+
+const Header = async({isAdminPage=false}) => {
+    const isAdmin = false;
+  return (
+    <header className="">
+     <nav>
+        <Link href={isAdminPage ? "/admin" : "/"}>
+            <Image src={"/logo.png"} alt='Vehiql Logo'
+            width={200}
+            height={60}
+            className='h-12 w-auto object-contain'/>
+
+            {isAdminPage && (
+                <span className="text-xs font-extralight">admin</span>
+            )}
+        </Link>
+     </nav>
+    </header>
+  );
+};
+
+export default Header;
